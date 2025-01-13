7d257da70ef70f83bc506147e2b2dc08d09bae6a
Selemani <lusukama.s@gmail.com>
2025-01-13 14:59:14 +0200

    Add Basci select query

---
+++ Lesson1/Basic-queries-select-data-from-tables.sql
@@ -0,0 +1,15 @@
+-- Select Customers table
+SELECT * FROM Customers;
+
+-- Select Products table
+SELECT * FROM Products;
+
+-- Select  Orders table
+SELECT * FROM Orders;
+
+-- Select  OrderDetails table
+SELECT * FROM OrderDetails;
+
+-- Select Employees table
+SELECT * FROM Employees;
+
